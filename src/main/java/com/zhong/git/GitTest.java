001c2cd05fdbb48047824842939842e0abbee715
zhong <1759397117@qq.com>
2022-04-14 13:54:02 +0800

    first commit

---
+++ src/main/java/com/zhong/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zhong.git;
+
+/**
+ * @author : zhong
+ * @Description :
+ * @create : 2022/4/14-13:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
