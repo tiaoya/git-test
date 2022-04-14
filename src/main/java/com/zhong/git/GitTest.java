d17ad32146bf479fbcd8925da24703796fe975a3
zhong <1759397117@qq.com>
2022-04-14 14:11:23 +0800

    master commit2

--- src/main/java/com/zhong/git/GitTest.java
+++ src/main/java/com/zhong/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello github!");
         System.out.println("hello github2!");
         System.out.println("hello git2");
+        System.out.println("master test");
     }
 }
